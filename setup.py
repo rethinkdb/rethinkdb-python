4ed593fa6d8025da4785fc2a0a375152ae70655c
Boros Gábor <gabor.boros@prezi.com>
2018-07-12 13:09:50 +0200

    Adding tox and pytest configuration. Also extend setup.py

--- setup.py
+++ setup.py
@@ -24,6 +24,15 @@ else:
         conditionalPackages = ['rethinkdb.asyncio_net']
     except ImportError: pass
 
+setup_requirements = [
+    'pytest-runner'
+]
+
+test_requirements = [
+    'pytest'
+]
+
+
 setuptools.setup(
     name="rethinkdb",
     zip_safe=True,
@@ -33,6 +42,16 @@ setuptools.setup(
     url="http://rethinkdb.com",
     maintainer="RethinkDB Inc.",
     maintainer_email="bugs@rethinkdb.com",
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
     packages=[
         'rethinkdb',
         'rethinkdb.tornado_net',
@@ -52,5 +71,8 @@ setuptools.setup(
             'rethinkdb-index-rebuild = rethinkdb._index_rebuild:main',
             'rethinkdb-repl = rethinkdb.__main__:startInterpreter'
         ]
-    }
+    },
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements
 )
