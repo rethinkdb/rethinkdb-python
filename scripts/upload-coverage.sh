89c9d28a24bb74532a63fe887475f6d04cbc1223
Adam Grandquist <grandquista@gmail.com>
2018-11-13 08:24:04 -0800

    Add upload script with optional test URL.
    For regular jobs this will upload to the staging pypi.
    On master it will do a real upload.
    Any upload is also tested by attempting to install the uploaded package.

--- scripts/upload-coverage.sh
+++ scripts/upload-coverage.sh
@@ -1,8 +1,14 @@
 if [ "${TRAVIS_PULL_REQUEST}" != "" ]; then
-    if [ "${CODACY_PROJECT_TOKEN}" = "" ]; then
-        echo "Skipping coverage upload for PR or missing CODACY_PROJECT_TOKEN"
-        exit;
-    fi
+    echo "Skipping coverage upload for PR"
+    exit;
 fi
+
+if [ "${CODACY_PROJECT_TOKEN}" = "" ]; then
+    echo "Skipping coverage upload for missing CODACY_PROJECT_TOKEN"
+    exit;
+fi
+
+set -ex
+
 pytest -m unit --cov rethinkdb --cov-report xml
 python-codacy-coverage -r coverage.xml
