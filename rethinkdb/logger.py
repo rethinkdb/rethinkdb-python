96b991c6d0a78f9f68de33522232293c2a93d866
Pavel Borisov <pzinin@gmail.com>
2019-01-13 01:23:38 +0300

    logging.basicConfig assumes that log_format is a str

--- rethinkdb/logger.py
+++ rethinkdb/logger.py
@@ -35,7 +35,7 @@ class DriverLogger(object):
         """
 
         super(DriverLogger, self).__init__()
-        log_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        log_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
         logging.basicConfig(format=log_format)
 
         self.logger = logging.getLogger()
