7bc04f4e6526347a47279dfc58ad31ec3704d5bd
Gábor Boros <gabor.brs@gmail.com>
2019-01-18 07:36:03 +0100

    add missing script

---
+++ scripts/install-db.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -euo pipefail
+
+export DISTRIB_CODENAME=$(lsb_release -sc)
+
+echo "deb https://dl.bintray.com/rebirthdb/apt $DISTRIB_CODENAME main" | sudo tee /etc/apt/sources.list.d/rebirthdb.list
+wget -qO- https://dl.bintray.com/rebirthdb/keys/pubkey.gpg | sudo apt-key add -
+
+sudo apt-get update --option Acquire::Retries=100 --option Acquire::http::Timeout="300"
+sudo apt-get --allow-unauthenticated install rebirthdb --option Acquire::Retries=100 --option Acquire::http::Timeout="300"
