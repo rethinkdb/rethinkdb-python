c989af98de36d7c2581bd5a95bb8dafe1b336ec9
Boros Gábor <gabor.brs@gmail.com>
2019-01-18 08:38:10 +0100

    Update upload-coverage.sh

--- scripts/upload-coverage.sh
+++ scripts/upload-coverage.sh
@@ -3,11 +3,6 @@
 set -e
 set -u
 
-if [ "${TRAVIS_PULL_REQUEST}" != "" ]; then
-    echo "Skipping coverage upload for PR"
-    exit;
-fi
-
 if [ "${CODACY_PROJECT_TOKEN}" = "" ]; then
     echo "Skipping coverage upload for missing CODACY_PROJECT_TOKEN"
     exit;
