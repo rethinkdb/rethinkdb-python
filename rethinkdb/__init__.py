d9b8cdf89bf1556704ae89eab9fb5c582f7ad9ed
Adam Grandquist <grandquista@gmail.com>
2019-02-28 06:44:13 -0800

    Refactor set loop type: closes #84 (#86)
    * set_loop_type supports reverting back to synchronous code
    
    * Refactor for clarity on imports and method aliasing.

--- rethinkdb/__init__.py
+++ rethinkdb/__init__.py
@@ -11,8 +11,13 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import os
+
+import imp
 
 from rethinkdb import errors, version
+from rethinkdb import net
+import pkg_resources
 
 
 # The builtins here defends against re-importing something obscuring `object`.
@@ -43,3 +48,31 @@ class RethinkDB(builtins.object):
         for module in (net, query, ast, errors):
             for function_name in module.__all__:
                 setattr(self, function_name, getattr(module, function_name))
+
+        self.set_loop_type(None)
+
+    def set_loop_type(self, library=None):
+        if library is None:
+            self.connection_type = net.DefaultConnection
+            return
+
+        # find module file
+        manager = pkg_resources.ResourceManager()
+        libPath = '%(library)s_net/net_%(library)s.py' % {'library': library}
+        if not manager.resource_exists(__name__, libPath):
+            raise ValueError('Unknown loop type: %r' % library)
+
+        # load the module
+        modulePath = manager.resource_filename(__name__, libPath)
+        moduleName = 'net_%s' % library
+        moduleFile, pathName, desc = imp.find_module(moduleName, [os.path.dirname(modulePath)])
+        module = imp.load_module('rethinkdb.' + moduleName, moduleFile, pathName, desc)
+
+        # set the connection type
+        self.connection_type = module.Connection
+
+        # cleanup
+        manager.cleanup_resources()
+
+    def connect(self, *args, **kwargs):
+        return self.make_connection(self.connection_type, *args, **kwargs)
