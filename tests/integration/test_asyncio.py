dc337bbc5bf391831ba50cbfae710ad38503a20a
Michal Mazurek <michal_mazurek@epam.com>
2019-03-04 10:53:42 +0000

    fixing asyncio tests to python >=3.4

--- tests/integration/test_asyncio.py
+++ tests/integration/test_asyncio.py
@@ -1,8 +1,10 @@
-import pytest
 import os
+import sys
+from collections import namedtuple
+from asyncio import coroutine
+import pytest
 from rethinkdb import RethinkDB
 from rethinkdb.errors import ReqlRuntimeError
-from collections import namedtuple
 
 Helper = namedtuple("Helper", "r connection")
 
@@ -30,9 +32,11 @@ async def rethinkdb_helper():
 
 
 @pytest.mark.integration
+@pytest.mark.skipif(sys.version_info < (3, 5),
+                    reason="requires python3.5 or higher")
 async def test_flow(rethinkdb_helper):
 
-    r: RethinkDB = rethinkdb_helper.r
+    r = rethinkdb_helper.r
     connection = rethinkdb_helper.connection
 
     await r.table_create("marvel").run(connection)
@@ -46,4 +50,29 @@ async def test_flow(rethinkdb_helper):
 
     cursor = await marvel_heroes.run(connection)
     async for hero in cursor:
+        assert hero['name'] == 'Iron Man'
+
+
+@pytest.mark.integration
+@pytest.mark.skipif(sys.version_info < (3, 4),
+                    reason="requires python3.4")
+@coroutine
+def test_flow_couroutine_paradigm(rethinkdb_helper):
+
+    r = rethinkdb_helper.r
+    connection = rethinkdb_helper.connection
+
+    yield from r.table_create("marvel").run(connection)
+
+    marvel_heroes = r.table('marvel')
+    yield from marvel_heroes.insert({
+        'id': 1,
+        'name': 'Iron Man',
+        'first_appearance': 'Tales of Suspense #39'
+    }).run(connection)
+
+    cursor = yield from marvel_heroes.run(connection)
+
+    while (yield from cursor.fetch_next()):
+        hero = yield from cursor.__anext__()
         assert hero['name'] == 'Iron Man'
