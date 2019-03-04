f8dea1dbc453e1921493820a090f3faa35ceef56
Michal Mazurek <michal_mazurek@epam.com>
2019-03-04 11:24:53 +0000

    separating coroutine paradigm and allow fail for python2.7 and 3.4

--- tests/integration/test_asyncio.py
+++ tests/integration/test_asyncio.py
@@ -1,7 +1,6 @@
 import os
 import sys
 from collections import namedtuple
-from asyncio import coroutine
 import pytest
 from rethinkdb import RethinkDB
 from rethinkdb.errors import ReqlRuntimeError
@@ -50,29 +49,4 @@ async def test_flow(rethinkdb_helper):
 
     cursor = await marvel_heroes.run(connection)
     async for hero in cursor:
-        assert hero['name'] == 'Iron Man'
-
-
-@pytest.mark.integration
-@pytest.mark.skipif(sys.version_info < (3, 4),
-                    reason="requires python3.4")
-@coroutine
-def test_flow_couroutine_paradigm(rethinkdb_helper):
-
-    r = rethinkdb_helper.r
-    connection = rethinkdb_helper.connection
-
-    yield from r.table_create("marvel").run(connection)
-
-    marvel_heroes = r.table('marvel')
-    yield from marvel_heroes.insert({
-        'id': 1,
-        'name': 'Iron Man',
-        'first_appearance': 'Tales of Suspense #39'
-    }).run(connection)
-
-    cursor = yield from marvel_heroes.run(connection)
-
-    while (yield from cursor.fetch_next()):
-        hero = yield from cursor.__anext__()
         assert hero['name'] == 'Iron Man'
