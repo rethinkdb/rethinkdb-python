fa27dac73ef30755c6d74335544e0932eab9f921
Mark E. Haase <mehaase@gmail.com>
2019-03-21 10:07:20 -0400

    Fix trio driver and add regression test

---
+++ tests/integration/test_trio.py
@@ -0,0 +1,50 @@
+import os
+import sys
+from collections import namedtuple
+import pytest
+from rethinkdb import RethinkDB
+from rethinkdb.errors import ReqlRuntimeError
+import trio
+
+INTEGRATION_TEST_DB = 'integration_test'
+r = RethinkDB()
+r.set_loop_type('trio')
+
+
+@pytest.fixture
+async def integration_db(nursery):
+    async with r.open(db='test', nursery=nursery) as conn:
+        try:
+            await r.db_create(INTEGRATION_TEST_DB).run(conn)
+        except ReqlRuntimeError:
+            pass
+    yield r.db(INTEGRATION_TEST_DB)
+
+
+@pytest.fixture
+async def marvel_table(integration_db, nursery):
+    async with r.open(db='test', nursery=nursery) as conn:
+        await r.table_create('marvel').run(conn)
+        yield r.table('marvel')
+        await r.table_drop('marvel').run(conn)
+
+
+@pytest.mark.trio
+@pytest.mark.integration
+@pytest.mark.skipif(sys.version_info < (3, 6),
+                    reason="Async generators require python ≥ 3.6")
+async def test_trio(marvel_table, nursery):
+    """
+    Test the flow for 3.6 and up, async generators are
+    not supported in 3.5.
+    """
+    async with r.open(db='test', nursery=nursery) as conn:
+        await marvel_table.insert({
+            'id': 1,
+            'name': 'Iron Man',
+            'first_appearance': 'Tales of Suspense #39'
+        }).run(conn)
+
+        cursor = await marvel_table.run(conn)
+        async for hero in cursor:
+            hero['name'] == 'Iron Man'
