34595aadd5d4739f5e3e9c84df421f640cd133e8
codeskyblue <codeskyblue@gmail.com>
2019-04-09 21:43:39 +0800

    add tests

---
+++ tests/integration/test_tornado.py
@@ -0,0 +1,28 @@
+import os
+import sys
+from collections import namedtuple
+import pytest
+from rethinkdb import RethinkDB
+from rethinkdb.errors import ReqlRuntimeError
+
+Helper = namedtuple("Helper", "r connection")
+
+INTEGRATION_TEST_DB = 'integration_test'
+
+
+@pytest.mark.integration
+@pytest.mark.skipif(sys.version_info < (3, 6),
+                    reason="requires python3.6 or higher")
+async def test_tornado_connect():
+    """
+    Test the flow for 3.6 and up, async generators are
+    not supported in 3.5.
+    """
+
+    r = RethinkDB()
+    r.set_loop_type("tornado")
+
+    connection = await r.connect(os.getenv("REBIRTHDB_HOST"))
+    dbs = await r.db_list().run(connection)
+    assert isinstance(dbs, list)
+    await connection.close()
