ef783c305ac7af68639ffc9d4cac91aa20c1b799
codeskyblue <codeskyblue@gmail.com>
2019-04-10 14:37:50 +0800

    fix travis test again

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,10 @@
 import sys
 
 collect_ignore = []
-if sys.version_info < (3, 4):
+
+if sys.version_info < (3, 6):
     collect_ignore += ["integration/test_asyncio.py",
-                       "integration/test_asyncio_coroutine.py",
                        "integration/test_tornado.py"]
-elif sys.version_info < (3, 6):
-    collect_ignore.append("integration/test_asyncio.py")
+
+if sys.version_info < (3, 4):
+    collect_ignore += ["integration/test_asyncio_coroutine.py"]
