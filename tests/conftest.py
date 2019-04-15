cc88050571852a5a9be749534148435e73aa4dca
Boros Gábor <gabor.brs@gmail.com>
2019-04-15 09:07:15 +0200

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,12 +3,10 @@ import sys
 collect_ignore = []
 
 if sys.version_info < (3, 6):
-    collect_ignore += ["integration/test_asyncio.py",
-                       "integration/test_tornado.py"]
-
-if sys.version_info < (3, 4):
-    collect_ignore += ["integration/test_asyncio.py", "integration/test_asyncio_coroutine.py"]
-elif sys.version_info < (3, 6):
-    collect_ignore.append("integration/test_asyncio.py")
-    collect_ignore.append("integration/test_trio.py")
+    collect_ignore += [
+        "integration/test_asyncio.py",
+        "integration/test_asyncio_coroutine.py",
+        "integration/test_tornado.py",
+        "integration/test_trio.py",
+    ]
     
