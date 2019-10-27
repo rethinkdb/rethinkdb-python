ce3b60c1e133888e5d4c1e80baa07914507adde0
Gábor Boros <gabor.brs@gmail.com>
2019-10-27 08:33:16 +0100

    Fix install script

--- scripts/install-db.sh
+++ scripts/install-db.sh
@@ -5,7 +5,7 @@ set -u
 
 export DISTRIB_CODENAME=$(lsb_release -sc)
 
-source /etc/lsb-release && echo "deb https://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
+echo "deb https://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
 wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add -
 
 sudo apt-get update --option Acquire::Retries=100 --option Acquire::http::Timeout="300"
