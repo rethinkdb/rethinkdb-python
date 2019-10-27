a7c2f6c3700c23dbe99d4b58ea738b93715e1619
Gábor Boros <gabor.brs@gmail.com>
2019-10-27 11:40:11 +0100

    add self.r

--- tests/helpers.py
+++ tests/helpers.py
@@ -9,20 +9,21 @@ class IntegrationTestCaseBase(object):
     conn = None
 
     def connect(self):
-        self.conn = r.connect(
+        self.conn = self.r.connect(
             host=self.rethinkdb_host
         )
 
     def setup_method(self):
-        self.rethinkdb_host=os.getenv('RETHINKDB_HOST')
+        self.r = r
+        self.rethinkdb_host = os.getenv('RETHINKDB_HOST')
 
         self.connect()
 
-        if INTEGRATION_TEST_DB not in r.db_list().run(self.conn):
-            r.db_create(INTEGRATION_TEST_DB).run(self.conn)
+        if INTEGRATION_TEST_DB not in self.r.db_list().run(self.conn):
+            self.r.db_create(INTEGRATION_TEST_DB).run(self.conn)
 
         self.conn.use(INTEGRATION_TEST_DB)
 
     def teardown_method(self):
-        r.db_drop(INTEGRATION_TEST_DB).run(self.conn)
+        self.r.db_drop(INTEGRATION_TEST_DB).run(self.conn)
         self.conn.close()
