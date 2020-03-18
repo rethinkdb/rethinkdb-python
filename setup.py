f4d790d15433b53d95052a83a821fdb0f557adfe
Gábor Boros <gabor.boros@gmail.com>
2020-03-18 08:05:49 +0100

    Remove python <3.6 dependencies

--- setup.py
+++ setup.py
@@ -66,10 +66,7 @@ setuptools.setup(
     classifiers=[
         'Intended Audience :: Developers',
         'Natural Language :: English',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
