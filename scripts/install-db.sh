7cc8b772772b6c806f9294382455115a40206401
Boros Gábor <gabor.brs@gmail.com>
2020-06-15 09:03:00 +0200

    Add missing sudo

--- scripts/install-db.sh
+++ scripts/install-db.sh
@@ -5,7 +5,7 @@ set -u
 
 export DISTRIB_CODENAME=$(lsb_release -sc)
 
-apt-key adv --keyserver keys.gnupg.net --recv-keys "539A 3A8C 6692 E6E3 F69B 3FE8 1D85 E93F 801B B43F"
+sudo apt-key adv --keyserver keys.gnupg.net --recv-keys "539A 3A8C 6692 E6E3 F69B 3FE8 1D85 E93F 801B B43F"
 echo "deb https://download.rethinkdb.com/repository/ubuntu-xenial xenial" > /etc/apt/sources.list.d/rethinkdb.list
 
 sudo apt-get update --option Acquire::Retries=100 --option Acquire::http::Timeout="300"
