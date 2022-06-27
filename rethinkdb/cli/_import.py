2c5951a206e5cfe31f956c47c8244254d92dea3f
Gábor Boros <gabor.brs@gmail.com>
2022-06-27 10:07:03 +0200

    feat: add command base (#279)
    Signed-off-by: Gabor Boros <gabor.brs@gmail.com>

---
+++ rethinkdb/cli/_import.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+# Copyright 2022 RethinkDB
+#
+# Licensed under the Apache License, Version 2.0 (the 'License');
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an 'AS IS' BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# This file incorporates work covered by the following copyright:
+# Copyright 2010-2016 RethinkDB, all rights reserved.
+
+"""
+Import loads data into a RethinkDB cluster.
+"""
+
+import click
+
+
+@click.command
+def cmd_import():
+    """
+    Import loads data into a RethinkDB cluster.
+    """
+    click.echo("import command")
